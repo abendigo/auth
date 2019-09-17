4b7503e95e5ca3b628003fe7357f426fff4e2e5d
Mark Oosterveld <mark@oosterveld.org>
2019-09-16 22:47:34 -0400

    progress

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const bodyParser = require('body-parser');
+const cookies = require('cookie-parser');
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
@@ -6,9 +8,24 @@ const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (request, response) => response.send('Hello World!'));
-app.get('/auth', (request, response) => response.sendStatus(204));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookies());
+
+app.post('/login/', (request, response) => {
+  console.log({ request });
+  response.cookie('mycookie', 'value');
+  response.sendStatus(200);
+});
+app.get('/auth', (request, response) => {
+  console.log('cookies', request.cookies);
+  const { mycookie } = request.cookies;
+  if (mycookie) {
+    return response.sendStatus(204);
+  }
+  return response.redirect('/login');
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => console.log('Listening'));
